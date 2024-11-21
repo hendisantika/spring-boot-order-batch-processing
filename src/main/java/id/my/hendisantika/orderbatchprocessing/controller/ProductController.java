077d1f36efd04ce8bd94d68913e89dca181fd960
hendisantika <hendisantika@yahoo.co.id>
2024-11-22 05:49:03 +0700

    Add processProductIds in ProductController🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/orderbatchprocessing/controller/ProductController.java
+++ src/main/java/id/my/hendisantika/orderbatchprocessing/controller/ProductController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -41,5 +42,11 @@ public class ProductController {
     public ResponseEntity<String> resetProductRecords() {
         String response = productService.resetRecords();
         return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/process")
+    public ResponseEntity<String> processProductIds(@RequestBody List<Long> productIds) {
+        productService.processProductIds(productIds);
+        return ResponseEntity.ok("Products processed and events published.");
     }
 }
