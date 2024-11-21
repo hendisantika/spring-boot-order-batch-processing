cc5371f2f870e1f8e8045440a05edf43064d3b2d
hendisantika <hendisantika@yahoo.co.id>
2024-11-22 05:38:44 +0700

    Add KafkaConfig🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/orderbatchprocessing/config/KafkaConfig.java
@@ -0,0 +1,29 @@
+package id.my.hendisantika.orderbatchprocessing.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-order-batch-processing
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 22/11/24
+ * Time: 05.38
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+public class KafkaConfig {
+
+    @Value("${product.discount.update.topic}")
+    private String topicName;
+
+
+    @Bean
+    public NewTopic createTopic() {
+        return new NewTopic(topicName, 3, (short) 1);
+    }
+}
