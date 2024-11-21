bee1fcc445eefa45528cad7c6f132dc8990a7904
hendisantika <hendisantika@yahoo.co.id>
2024-11-22 05:44:42 +0700

    Add ProductServiceV2🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/orderbatchprocessing/service/ProductServiceV2.java
@@ -0,0 +1,35 @@
+package id.my.hendisantika.orderbatchprocessing.service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import id.my.hendisantika.orderbatchprocessing.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-order-batch-processing
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 22/11/24
+ * Time: 05.43
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+@RequiredArgsConstructor
+public class ProductServiceV2 {
+
+    private final ProductRepository repository;
+    private final KafkaTemplate<String, String> kafkaTemplate;
+    private final ObjectMapper objectMapper;
+    @Value("${product.discount.update.topic}")
+    private final String topicName;
+
+    private final ExecutorService executorService = Executors.newFixedThreadPool(6);
+
+}
