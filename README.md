f61aa8ac166c6d0accad1ca89e3b371d7994ec68
hendisantika <hendisantika@yahoo.co.id>
2024-11-22 05:35:29 +0700

    Create Spring Boot🫸🌀✏️📗🐧🐳⬆

---
+++ README.md
@@ -0,0 +1,1 @@
+# spring-boot-order-batch-processing
---
+++ src/test/java/id/my/hendisantika/orderbatchprocessing/SpringBootOrderBatchProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.orderbatchprocessing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootOrderBatchProcessingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
